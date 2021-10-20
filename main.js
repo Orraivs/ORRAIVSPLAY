f540105cfda5331e53d389af87c392be104739f1
David Celis <davidcelis682@gmail.com>
2021-10-20 12:09:58 -0500

    Update

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function createWindow () {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
+      minWidth: 800,
+      minHeight: 610,
       /*webPreferences: {
         preload: path.join(__dirname, '/src/js/preload.js')
       }*/
