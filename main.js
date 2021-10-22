444005ef50f9c89a472b31500011be4f87538c40
SneyderRubio <esneiderrubio3107@gmail.com>
2021-10-21 21:44:00 -0500

    update

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
-      height: 600,
-      minWidth: 800,
-      minHeight: 610,
-      /*webPreferences: {
-        preload: path.join(__dirname, '/src/js/preload.js')
-      }*/
+      height: 600
     })
+  
     win.loadFile(__dirname + '/src/views/signin.html')
-  }
 
-app.whenReady().then(() => {
+  }
+  app.whenReady().then(() => {
     createWindow()
   })
-
   app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
   })
